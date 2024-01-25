6232a874d191e1b75d9f8899ee1c4983381caf9e
Nusri7 <321nusri@gmail.com>
2024-01-25 06:52:09 +0530

    Project initialize

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Peoples Bank chatbot usin OpenAI',
+    version= '1.0.0',
+    author= 'Ahamed Nusri',
+    author_email= '321nusri@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
